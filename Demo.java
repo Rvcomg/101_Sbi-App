5b3b60a567ccb32cb561a88909582676abd175b3
unknown <rv@example.com>
2023-03-11 12:19:30 +0530

    bug 190 fixed

--- Demo.java
+++ Demo.java
@@ -1,9 +1,17 @@
 public class Demo{
+   //bug -190 changes
+    public void doprocess(){
+	return ;
+	}
 
+<<<<<<< Updated upstream
 	// bug fixed 193
 	public void m1(){
 
 	}
+=======
+      
+>>>>>>> Stashed changes
 
 
 }
